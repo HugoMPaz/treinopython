7470a38d61859ac0df47e432397581acfa302c1c
hugo <hugompaz17@gmail.com>
2022-03-18 15:44:38 -0300

    app teste

---
+++ app.py
@@ -0,0 +1,10 @@
+numero1 = int (input ('digite um numero qualquer '))
+numero2 = int (input ('digite outro numero qualquer '))
+soma = numero1 + numero2
+if numero1 > numero2:
+    print('o numero 1 é maior que o numero 2')
+else:
+    print('o numero 1 é menor que o numero 2')
+
+
+print('a soma dos dois numeros é: ', soma)
